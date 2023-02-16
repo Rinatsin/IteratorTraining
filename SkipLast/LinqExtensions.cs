785996d099a0eab019d18fb066198259a699dffc
salimyanov <r.salimyanov@onh-it.ru>
2023-02-16 14:58:01 +0300

    Добавьте файлы проекта.

---
+++ SkipLast/LinqExtensions.cs
@@ -0,0 +1,44 @@
+﻿namespace SkipLast
+{
+	internal static class LinqExtensions
+	{
+		public static IEnumerable<T> SkipLastNItems<T>(this IEnumerable<T> source, int n)
+		{
+			var iterator = source.GetEnumerator();
+			var hasItems = false;
+			var cache = new Queue<T>(n + 1);
+
+			do
+			{
+				if (hasItems = iterator.MoveNext())
+				{
+					cache.Enqueue(iterator.Current);
+					if (cache.Count > n)
+					{
+						yield return cache.Dequeue();
+					}
+				}
+
+			} while (hasItems);
+		}
+
+		public static IEnumerable<T> SkipLastItem<T>(this IEnumerable<T> source)
+		{
+			var iterator = source.GetEnumerator();
+			var hasItems = false;
+			var isFirst = true;
+			var item = default(T);
+
+			do
+			{
+				hasItems = iterator.MoveNext();
+				if (hasItems)
+				{
+					if (!isFirst) yield return item;
+					item = iterator.Current;
+					isFirst = false;
+				}
+			} while (hasItems);
+		}
+	}
+}
---
+++ SkipLast/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SkipLast
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+
+			var seq = new int[5] { 1, 2, 3, 4, 5 };
+			var result = seq.SkipLastNItems(2);
+
+			Console.WriteLine(string.Join(", ", seq.Select(x => x.ToString())));
+			Console.WriteLine(string.Join(", ", result.Select(x => x.ToString())));
+			Console.ReadLine();
+		}
+
+	}
+}
